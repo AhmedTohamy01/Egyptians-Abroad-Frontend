11be2b9c2cfbd984cb2fc2bf63b7c1ba1956b4f8
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-18 18:47:19 +0200

    feat: add signup-navbar

---
+++ components/Navbar/SignupNavbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import Link from 'next/link'
+
+/*---> Component <---*/
+function SignNavbar({ ...restProps }) {
+  return (
+    <NavbarWrapper>
+      <LogoWrapper>
+        <Logo>EgyptiansAbroad</Logo>
+      </LogoWrapper>
+    </NavbarWrapper>
+  )
+}
+
+/*---> Styles <---*/
+
+export const NavbarWrapper = styled.div`
+  /* border: 1px solid black; */
+  height: 70px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const LogoWrapper = styled.div`
+  /* border: 1px solid yellow; */
+`
+
+export const Logo = styled.p`
+  /* border: 1px solid yellow; */
+  font-size: 40px;
+  font-family: 'Caveat', cursive;
+  font-weight: 700;
+  color: #1399ff;
+`
+
+export default SignNavbar
