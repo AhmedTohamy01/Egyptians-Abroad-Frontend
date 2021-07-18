1211b7ebb9b25e85f4966250e6b4ef78ae5334a3
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-18 19:38:34 +0200

    feat: add sample home page

---
+++ pages/home.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <>
+      <Head>
+        <title>Egyptians Abroad</title>
+        <meta charSet='utf-8' />
+        <meta
+          name='description'
+          content='website to connect egyptians abroad and answer their questions'
+        />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      This is home page!
+    </>
+  )
+}
