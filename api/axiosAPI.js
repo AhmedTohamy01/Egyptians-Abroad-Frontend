a4beaaa91e17cb9d5cf87e7c7060aa3b4770befe
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-19 01:25:31 +0200

    feat: add getUserInfo & getUserAvatar functions to axiosAPi | add authorization to headers

--- api/axiosAPI.js
+++ api/axiosAPI.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
+let token = ''
+// this code because Next.js don't give access to window object in SSR mode.
+if (typeof window !== 'undefined') {
+  token = window.localStorage.getItem('EgAbroadToken')
+  // here we can access window object
+}
+
 const getBaseUrl = () => {
   return 'http://localhost:5000'
 }
@@ -9,6 +16,7 @@ const apiClient = axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
   },
 })
 
@@ -20,6 +28,12 @@ export default {
     login(payload) {
       return apiClient.post('/users/login', payload)
     },
+    getUserInfo() {
+      return apiClient.get('/users/me')
+    },
+		getUserAvatar(userId) {
+			return `${getBaseUrl()}/users/${userId}/avatar`
+		}
   },
   post: {},
   comment: {},
