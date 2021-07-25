479f37c3278177e66e6e942878d94425fea7763a
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-25 04:40:07 +0200

    feat: add HomeBody component

---
+++ components/Home/HomeBody.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useContext } from 'react'
+import styled from 'styled-components'
+import axiosAPI from '../../api/axiosAPI'
+import GenericCard from '../Post/GenericCard'
+import Button from '@material-ui/core/Button'
+
+/*---> Component <---*/
+export default function HomeBody() {
+  const [error, setError] = useState(null)
+  const [allPosts, setAllPosts] = useState([])
+  const [limit, setLimit] = useState(20)
+  const [skip, setSkip] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(async () => {
+    try {
+      const allNewPosts = await axiosAPI.post.getAllPosts(limit, skip)
+      const arr = allPosts.concat(allNewPosts.data)
+
+      arr.forEach((item) => {
+        const avatar = axiosAPI.user.getUserAvatar(item.owner)
+        item.avatarLink = avatar
+      })
+      setAllPosts(arr)
+      setLoading(false)
+    } catch (e) {
+      setError(e)
+    }
+  }, [limit, skip])
+
+  function handleShowMore() {
+    setSkip(skip + 5)
+  }
+
+  function isValidImage(url) {
+    let image = new Image()
+    image.src = url
+    if (image.width > 0 || image.height > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  if (loading) {
+    return <>loading ...</>
+  }
+
+  return (
+    <ProfileBodyWrapper>
+      <PostsTitle>All Posts</PostsTitle>
+      <PostsWrapper>
+        {allPosts.map((item) => (
+          <GenericCard
+            key={item._id}
+            ownerId={item.owner}
+            title={item.title}
+            src={
+              isValidImage(item.avatarLink)
+                ? item.avatarLink
+                : '/images/avatar.png'
+            }
+          />
+        ))}
+      </PostsWrapper>
+      <ShowMoreButton onClick={handleShowMore}>Show More</ShowMoreButton>
+    </ProfileBodyWrapper>
+  )
+}
+
+/*---> Styles <---*/
+export const ProfileBodyWrapper = styled.div`
+  /* border: solid red; */
+  max-width: 460px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+  min-height: calc((100vh) * 0.25);
+`
+
+export const PostsWrapper = styled.div`
+  /* border: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`
+
+export const PostsTitle = styled.div`
+  /* border: 1px solid green; */
+  font-size: 35px;
+  font-family: 'Caveat', cursive;
+  font-weight: 700;
+  color: #1399ff;
+`
+
+export const ShowMoreButton = styled(Button)`
+  background: linear-gradient(to bottom, #2bc9ff, #1399ff 100%) !important;
+  width: 125px !important;
+  height: 36px !important;
+  padding: 0 12px !important;
+  border-radius: 8px !important;
+  text-transform: initial !important;
+  color: white !important;
+  font-size: 16px !important;
+`
