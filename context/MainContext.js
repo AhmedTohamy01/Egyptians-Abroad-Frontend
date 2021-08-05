156d5f83aec9636a6ae1ef0be39d203e54c40322
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-05 23:22:26 +0200

    feat: change the that way of getting avatarLink in MainContext

--- context/MainContext.js
+++ context/MainContext.js
@@ -4,14 +4,9 @@ import getData from '../custom-hook/getData'
 export const MainContext = createContext([{}, () => {}])
 
 export const MainContextProvider = ({ children }) => {
-  const { userProfile, avatarURL } = getData()
+  const { userProfile, avatarLink } = getData()
   const [showMenuCard, setShowMenuCard] = useState(false)
   const [showProfileCard, setShowProfileCard] = useState(false)
-  const [avatarLink, setAvatarLink] = useState(null)
-  
-	useEffect(() => {
-		setAvatarLink(avatarURL)
-	})
 
   return (
     <MainContext.Provider
@@ -22,7 +17,6 @@ export const MainContextProvider = ({ children }) => {
         setShowProfileCard,
         userProfile,
         avatarLink,
-        setAvatarLink,
       }}
     >
       {children}
