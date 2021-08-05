2d04ffa3167e1ed0e2114004f6f4156f70c54585
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-05 06:51:34 +0200

    feat: add userProfile & avatarLink to MainContextProvider | remove unused code

--- context/MainContext.js
+++ context/MainContext.js
@@ -1,27 +1,22 @@
 import { createContext, useState, useEffect } from 'react'
+import getData from '../custom-hook/getData'
 
 export const MainContext = createContext([{}, () => {}])
 
 export const MainContextProvider = ({ children }) => {
+	const { userProfile, avatarLink } = getData()
   const [showMenuCard, setShowMenuCard] = useState(false)
-  const [activeSignForm, setActiveSignForm] = useState('signup')
-  const [activeGettingStarted, setActiveGettingStarted] = useState('basic-info')
   const [showProfileCard, setShowProfileCard] = useState(false)
-	const [showAddCard, setShowAddCard] = useState(false)
 
   return (
     <MainContext.Provider
       value={{
         showMenuCard,
         setShowMenuCard,
-        activeSignForm,
-        setActiveSignForm,
-        activeGettingStarted,
-        setActiveGettingStarted,
         showProfileCard,
         setShowProfileCard,
-        showAddCard,
-        setShowAddCard,
+        userProfile,
+        avatarLink,
       }}
     >
       {children}
