b7dbaaad49eea27e55a36ff074174fc7e39b4800
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-07 05:13:01 +0200

    feat: add loading-spinner to HomePage

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,26 @@
+import React, { useContext, useState, useEffect } from 'react'
 import Head from 'next/head'
-import React from 'react'
 import HomeNavbar from '../components/Navbar/HomeNavbar'
 import PrivateRoute from '../custom-routes/PrivateRoute'
 import ProfileHeader from '../components/Profile/ProfileHeader'
 import HomeBody from '../components/Home/HomeBody'
 import Footer from '../components/Footer/Footer'
 import AddButton from '../components/Home/AddButton'
+import Loader from 'react-loader-spinner'
+import styled from 'styled-components'
+import { MainContext } from '../context/MainContext'
 
 export default function HomePage() {
+  const { userProfile, avatarLink } = useContext(MainContext)
+
+  if (!userProfile || !avatarLink) {
+    return (
+      <SpinnerWrapper>
+        <Loader type='ThreeDots' color='#1399ff' height={100} width={100} />
+      </SpinnerWrapper>
+    )
+  }
+
   return (
     <PrivateRoute>
       <Head>
@@ -25,11 +38,21 @@ export default function HomePage() {
           rel='stylesheet'
         />
       </Head>
-      <HomeNavbar />
-      <ProfileHeader />
+      <HomeNavbar userProfile={userProfile} avatarLink={avatarLink} />
+      <ProfileHeader userProfile={userProfile} avatarLink={avatarLink} />
       <HomeBody />
       <AddButton ButtonLink='/new-post' />
       <Footer />
     </PrivateRoute>
   )
 }
+
+/*---> Styles <---*/
+export const SpinnerWrapper = styled.div`
+  /* border: 1px solid red; */
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
