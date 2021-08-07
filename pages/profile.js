2a4de0cf74a77eab28d7a498f3895acd9b23d9c9
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-07 07:18:56 +0200

    feat: add loading-spinner in ProfilePage

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,25 @@
+import React, { useContext, useState, useEffect } from 'react'
 import PrivateRoute from '../custom-routes/PrivateRoute'
 import ProfileHeader from '../components/Profile/ProfileHeader'
 import HomeNavbar from '../components/Navbar/HomeNavbar'
 import Head from 'next/head'
 import Footer from '../components/Footer/Footer'
 import ProfileBody from '../components/Profile/ProfileBody'
+import { MainContext } from '../context/MainContext'
+import Loader from 'react-loader-spinner'
+import styled from 'styled-components'
 
 export default function ProfilePage() {
+  const { userProfile, avatarLink } = useContext(MainContext)
+
+	if (!userProfile || !avatarLink) {
+    return (
+      <SpinnerWrapper>
+        <Loader type='ThreeDots' color='#1399ff' height={100} width={100} />
+      </SpinnerWrapper>
+    )
+  }
+
   return (
     <>
       <Head>
@@ -24,11 +38,21 @@ export default function ProfilePage() {
         />
       </Head>
       <PrivateRoute>
-        <HomeNavbar />
-        <ProfileHeader />
-        <ProfileBody />
+        <HomeNavbar userProfile={userProfile} avatarLink={avatarLink} />
+        <ProfileHeader userProfile={userProfile} avatarLink={avatarLink} />
+        <ProfileBody avatarLink={avatarLink} />
         <Footer />
       </PrivateRoute>
     </>
   )
 }
+
+/*---> Styles <---*/
+export const SpinnerWrapper = styled.div`
+  /* border: 1px solid red; */
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
