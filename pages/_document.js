46d3497975fa186336cd717e878e7c05fca19ab1
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-08-27 03:50:08 +0200

    refactor: add <Head> tag in _app.js instead of _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -32,22 +32,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <title>Egyptians Abroad</title>
-          <meta charSet='utf-8' />
-          <meta
-            name='description'
-            content='website to connect egyptians abroad and answer their questions'
-          />
-          <meta
-            name='viewport'
-            content='initial-scale=1.0, width=device-width'
-          />
-          <link rel='icon' href='/favicon.ico' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap'
-            rel='stylesheet'
-          />
         </Head>
         <body>
           <Main />
